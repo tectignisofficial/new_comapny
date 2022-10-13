06d751ec275a189563aa6613a49da02abc9bd5cd
aniketnangare23 <aniketnangare23@gmail.com>
2022-10-13 18:36:54 +0530

    session

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,7 @@
 session_start();
 if(!isset($_SESSION['id']))
 {                                                                                       
-  header("location:adminlogin.php");
+  header("location:login.php");
 }
 
 ?>
