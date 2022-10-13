c19542d2a0378d83b0f7858826b47ef791c51469
aniketnangare23 <aniketnangare23@gmail.com>
2022-10-13 18:59:27 +0530

    login image

--- admin/login.php
+++ admin/login.php
@@ -90,7 +90,7 @@ else{
                         <!-- /Brand logo-->
                         <!-- Left Text-->
                         <div class="d-none d-lg-flex col-lg-8 align-items-center p-5">
-                            <div class="w-100 d-lg-flex align-items-center justify-content-center px-5"><img class="img-fluid" src="app-assets/images/pages/login-v2.svg" alt="Login V2" /></div>
+                            <div class="w-100 d-lg-flex align-items-center justify-content-center px-5"><img class="img-fluid" src="app-assets/images/login/login-v2.svg" alt="Login V2" /></div>
                         </div>
                         <!-- /Left Text-->
                         <!-- Login-->
