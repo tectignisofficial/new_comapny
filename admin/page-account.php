ad6be27ca9aea0d949bf2ad16de655cc9e22c5b8
whynaidu <naiduvedant@gmail.com>
2022-10-15 01:13:26 +0530

    tried favicon for Admin panel

--- admin/page-account.php
+++ admin/page-account.php
@@ -47,7 +47,7 @@ if(isset($_POST['submit'])){
     <meta name="author" content="PIXINVENT">
     <title>Account - Vuexy - Bootstrap HTML admin template</title>
     <link rel="apple-touch-icon" href="app-assets/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/ico/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="app-assets/images/ico/favicon.webp">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
         rel="stylesheet">
 
